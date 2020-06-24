6492a6fa19e5054eefbd1bfcd6d5f20e41ab4836
coaxial <hi@64b.it>
2020-06-24 08:56:06 +0200

    Cleanup code

--- extractors/fip.go
+++ extractors/fip.go
@@ -46,26 +46,52 @@ type FipHistoryResponse struct {
 }
 
 func (extractor FipExtractor) Playlist(timestampFrom int64) ([]playlist.Track, error) {
-	// FIP uses graphql to serve its tracks history. To get the history for any given date and time, issue a GET to www.fip.fr/latest/api/graphql with a query string containing the following:
+	// FIP uses graphql to serve its tracks history. To get the history for
+	// any given date and time, issue a GET to
+	// www.fip.fr/latest/api/graphql with a query string containing the
+	// following:
 	// operationName=History
-	// variables={"first": <int>, "after": <base64 encoded seconds epoch>, "stationgId": <int>}
-	// extensions={"persistedQuery: {"version": 1, "sha256Hash": "f8f404573583a6a9410cd24637f214a0b93038696c1d20f19202111b51fd8270"}
-	// It returns an extensive object containing `first` number of tracks played since `after` timestamp
+	// variables={
+	//   "first": <int>,
+	//   "after": <base64 encoded seconds epoch>,
+	//   "stationgId": <int>
+	// }
+	// extensions={
+	//   "persistedQuery: {
+	//     "version": 1,
+	//     "sha256Hash": "f8f404573583a6a9410cd24637f214a0b93038696c1d20f19202111b51fd8270"
+	//   }
+	// }
+	// It returns a FipHistoryResponse containing `first` number of tracks
+	// played since `after` timestamp
+	endpoint := "https://www.fip.fr/latest/api/graphql"
 	first := 10
 	from := time.Now().Unix()
 	timestamp := base64.StdEncoding.EncodeToString([]byte(strconv.FormatInt(from, 10)))
 	stationId := 7 // 7 is FIP
 
 	// Build URL with http.NewRequest so that the query string can be easily built
-	req, err := http.NewRequest("GET", "https://www.fip.fr/latest/api/graphql", nil)
+	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
-		// TODO should return an error instead
 		log.Fatal(err)
+		return nil, err
 	}
 	query := req.URL.Query()
 	query.Add("operationName", "History")
-	query.Add("variables", fmt.Sprintf("{\"first\":%d,\"after\":\"%s\",\"stationId\":%d}", first, timestamp, stationId))
-	query.Add("extensions", "{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"f8f404573583a6a9410cd24637f214a0b93038696c1d20f19202111b51fd8270\"}}")
+	query.Add(
+		"variables",
+		fmt.Sprintf(
+			"{\"first\":%d,\"after\":\"%s\",\"stationId\":%d}",
+			first,
+			timestamp,
+			stationId,
+		),
+	)
+	query.Add(
+		"extensions",
+		"{\"persistedQuery\":{\"version\":1,\"sha256Hash\":"+
+			"\"f8f404573583a6a9410cd24637f214a0b93038696c1d20f19202111b51fd8270\"}}",
+	)
 	req.URL.RawQuery = query.Encode()
 
 	// Build the client to make the request
@@ -73,23 +99,23 @@ func (extractor FipExtractor) Playlist(timestampFrom int64) ([]playlist.Track, e
 	response, err := client.Do(req)
 
 	if err != nil {
-		// TODO should return an error instead
 		log.Fatal(err)
+		return nil, err
 	}
 	// A response that isn't HTTP 200 OK will still make err nil, so a
 	// check needs to be done to make sure it succeeded
 	if response.StatusCode != http.StatusOK {
 		fmt.Printf("%#v", response)
-		// TODO should return an error instead
 		log.Fatal("Query didn't succeed")
+		return nil, err
 	}
 
 	defer response.Body.Close()
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		// TODO should return an error instead
 		log.Fatal(err)
+		return nil, err
 	}
 	var responseObject FipHistoryResponse
 	json.Unmarshal(responseData, &responseObject)
