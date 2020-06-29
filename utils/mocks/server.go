e15d47f88f9d97456639ea529752cbbe9cdd5166
coaxial <hi@64b.it>
2020-06-29 10:26:15 +0200

    Improve comments

--- utils/mocks/server.go
+++ utils/mocks/server.go
@@ -8,12 +8,15 @@ import (
 	"github.com/coaxial/tizinger/utils/logger"
 )
 
-// Server mocks the response.
+// Server spins up a new HTTP server to return canned responses. The handler
+// function processes the requests and eventually returns a fixture/canned
+// data.
 func Server(handler func(http.ResponseWriter, *http.Request)) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(handler))
 }
 
-// LoadFixture is for loading canned responses to use in handler functions.
+// LoadFixture is a helper for loading canned responses to use in handler
+// functions.
 func LoadFixture(path string) []byte {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
