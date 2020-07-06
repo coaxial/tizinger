7bb6f7390e3545635d0e6816355944b611d29cf0
coaxial <hi@64b.it>
2020-07-06 11:36:34 +0200

    Use implementation instead of interface

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/coaxial/tizinger/extractor"
+	"github.com/coaxial/tizinger/fip"
+	"github.com/coaxial/tizinger/utils/logger"
 )
 
 func main() {
-	var fipClient extractor.Client
-	list, _ := fipClient.Playlist(time.Date(2019, 07, 05, 0, 0, 0, 0, time.UTC).Unix(), 10)
-	fmt.Printf("%#v", list)
+	var fipClient fip.APIClient
+	ts := time.Date(2019, time.July, 5, 0, 0, 0, 0, time.UTC).Unix()
+	count := 300
+	list, err := fipClient.Playlist(ts, count)
+	if err != nil {
+		logger.Error.Fatal(err)
+	}
+
+	for _, v := range list {
+		fmt.Printf("Artist: %q, Album: %q, Title: %q\n", v.Artist, v.Album, v.Title)
+	}
 }
