4b8db1eebbd34b2f857e7a740f90f401974b306b
coaxial <c@64b.it>
2025-10-14 11:42:08 +0200

    fix: update example test to use mock fixture instead of live API

--- fip/client_test.go
+++ fip/client_test.go
@@ -87,16 +87,27 @@ func TestEmptyResponse(t *testing.T) {
 }
 
 func ExampleAPIClient_Playlist() {
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		resp.WriteHeader(http.StatusOK)
+		resp.Header().Set("Content-Type", "application/json; charset=utf-8")
+		length, historyJSON := mocks.LoadFixture("../fixtures/fip/history_response.json")
+		resp.Header().Set("Content-Length", strconv.Itoa(length))
+		resp.Write(historyJSON)
+	}
+	server := mocks.Server(http.HandlerFunc(handler))
+	defer server.Close()
+	SetEndpointURL(server.URL)
+	defer ResetEndpointURL()
+
 	var fipClient APIClient
-	// Get the list of 10 tracks played on FIP since 2020-07-25 00:30:00 GMT
-	tracks, err := fipClient.Playlist(1564014600, 10)
+	// Get the list of 10 tracks played on FIP since 2019-07-05 00:00:00 GMT (adjusted to match fixture)
+	tracks, err := fipClient.Playlist(1562284800, 10)
 	if err != nil {
 		log.Fatalf("Could not fetch FIP tracks: %v", err)
 	}
 
 	fmt.Println(tracks)
-	// Output: [{Riding the sun Howls Howls} {In the wake of adversity Dead Can Dance Within the realm of a dying sun} {Madame rêve Alain Bashung Osez Josephine} {The Planets op 32 : 3. Mercury, the Winged Messenger Orchestre Symphonique De Chicago Gustav Holst : Les Planètes} {Annie : The hard-knock life Alicia Morton BOF TV / Annie} {Bruce Lee Catastrophe Bruce Lee} {New comer 1 Walt Rockman Dusty fingers} {Cars Gary Numan The pleasure principle / Warriors} {Radio #1 Air 10000 hz legend} {Previsão do tempo Marcos Valle Previsao do tempo}]
-
+	// Output: [{Scar tissue Red Hot Chili Peppers Greatest hits} {Off the wall Jil Is Lucky Off the wall} {Kalimba (Flute mix) Freakniks Electro tunes} {Tsukikaage no rendezvous Keiko Mari Nippon girls: Japanese pop, beat & bossa nova 1966-1970} {Un petit poisson, un petit oiseau Juliette Greco Déshabillez-moi 1965-1969} {I want to be happy Ray Brown Brown Ray trio / Some of my best friends are guitarists} {I'm so happy I can't stop crying Sting Mercury falling} {Sambarilove (feat. Roubinho Jacobina) Chiara Civello Eclipse} {Retiens l'été Double Francoise Les bijoux} {Serenade nº13 en Sol Maj K 525 ""une petite musique de nuit"" : I. Allegro I Musici Mozart, pachelbel, albinoni}]
 }
 
 func TestEndCursorConvert(t *testing.T) {
